6612ba5a79c47b7266300ff045abbb74e8cdd54c
byron dray <byrondray8@gmail.com>
2024-09-25 18:15:48 -0700

    add graphql intregration to the frontend and generate types from the apis

---
+++ graphql/queries.ts
@@ -0,0 +1,22 @@
+// src/graphql/queries.ts
+import { gql } from '@apollo/client';
+
+export const GET_USERS = gql`
+  query GetUsers {
+    getUsers {
+      id
+      name
+      email
+    }
+  }
+`;
+
+export const GET_USER = gql`
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      name
+      email
+    }
+  }
+`;
