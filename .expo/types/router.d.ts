8951e679ffa9386397745281523012c7eb078aa2
JustArmaan <singharmaanbrar@gmail.com>
2024-10-26 13:39:06 -0700

    feature: create form input for requests

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/Carpool/createRide` | `/Carpool/createRide` | `/Login/login` | `/OnboardForms/child` | `/OnboardForms/parent` | `/OnboardForms/vehicle` | `/Register/register` | `/_sitemap` | `/theme`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/Carpool/createRide` | `/(tabs)/Carpool/postRequest` | `/Carpool/createRide` | `/Carpool/postRequest` | `/Login/login` | `/OnboardForms/child` | `/OnboardForms/parent` | `/OnboardForms/vehicle` | `/Register/register` | `/_sitemap` | `/theme`;
       DynamicRoutes: `/(tabs)/messages/${Router.SingleRoutePart<T>}` | `/messages/${Router.SingleRoutePart<T>}`;
       DynamicRouteTemplate: `/(tabs)/messages/[userId]` | `/messages/[userId]`;
     }
