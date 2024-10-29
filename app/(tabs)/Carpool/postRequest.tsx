9dc4a2210a753ea59278da1b2b795305ab26682e
JustArmaan <singharmaanbrar@gmail.com>
2024-10-29 14:56:41 -0700

    fix: fix for demo

--- app/(tabs)/Carpool/postRequest.tsx
+++ app/(tabs)/Carpool/postRequest.tsx
@@ -7,6 +7,7 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
+  StyleSheet,
 } from "react-native";
 import { ThemedAddressCompletionInput } from "@/components/ThemedAddressCompletionInput";
 import { useThemeColor } from "@/hooks/useThemeColor";
@@ -16,6 +17,8 @@ import {
   RadioGroup,
   RangeDatepicker,
   Button,
+  Layout,
+  Popover,
 } from "@ui-kitten/components";
 import { TextInput } from "react-native-gesture-handler";
 import { TimePickerModal } from "react-native-paper-dates";
@@ -29,6 +32,7 @@ const RequestRide = () => {
   const [range, setRange] = React.useState({});
   const [showTimePicker, setShowTimePicker] = useState(false);
   const [time, setTime] = useState("");
+  const [visible, setVisible] = React.useState(false);
 
   console.log("Range:", range);
 
@@ -42,6 +46,23 @@ const RequestRide = () => {
     setTime(`${hours}:${minutes}`);
     setShowTimePicker(false);
   };
+
+  const handleModelSubmit = () => {
+    setVisible(true);
+  };
+
+  const renderToggleButton = (): React.ReactElement => (
+    <Button
+      style={{
+        width: "100%",
+        paddingVertical: 12,
+      }}
+      appearance="ghost"
+      onPress={handleModelSubmit}
+    >
+      {() => <Text style={{ color: "#fff", fontSize: 16 }}>Submit</Text>}
+    </Button>
+  );
 
   return (
     <KeyboardAvoidingView
@@ -57,7 +78,7 @@ const RequestRide = () => {
       >
         <View>
           <Text style={{ fontSize: 32, fontWeight: "bold", marginBottom: 20 }}>
-            Post a ride
+            Post a request
           </Text>
         </View>
 
@@ -85,7 +106,7 @@ const RequestRide = () => {
           }}
           onLatLonSelect={(lat, lon) => {}}
           placeholder="Enter Origin"
-          style={{ marginBottom: 20 }}
+          style={{ marginBottom: 10 }}
         />
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
           <Text style={{ color: textColor, marginBottom: 5 }}>To</Text>
@@ -99,7 +120,7 @@ const RequestRide = () => {
           }}
           onLatLonSelect={(lat, lon) => {}}
           placeholder="Enter Origin"
-          style={{ marginBottom: 20 }}
+          style={{ marginBottom: 10 }}
         />
         <Text style={{ marginBottom: 5, marginTop: 15, color: "#8F9BB3" }}>
           Date Range of Ride
@@ -169,13 +190,17 @@ const RequestRide = () => {
         <Text style={{ color: textColor, marginBottom: 5, marginTop: 20 }}>
           Description
         </Text>
+
         <TextInput
           style={{
             width: "100%",
             backgroundColor: "#F7F9FC",
             borderColor: "#E4E9F2",
+            borderWidth: 1,
+            borderRadius: 15,
             height: 100,
-            paddingLeft: 10,
+            paddingLeft: 30,
+            paddingRight: 30,
           }}
           placeholder="Tell drivers more about any special arrangement, e.g. extra car seat, large instrument"
           multiline={true}
@@ -187,6 +212,7 @@ const RequestRide = () => {
             alignItems: "center",
             paddingHorizontal: 16,
             backgroundColor: "#fff",
+            marginTop: 40,
           }}
         >
           <LinearGradient
@@ -199,23 +225,46 @@ const RequestRide = () => {
               overflow: "hidden",
             }}
           >
-            <Button
-              style={{
-                width: "100%",
-                paddingVertical: 12,
-              }}
-              appearance="ghost"
-              onPress={() => console.log("Button pressed")}
-            >
-              {() => (
-                <Text style={{ color: "#fff", fontSize: 16 }}>Submit</Text>
-              )}
-            </Button>
+            {renderToggleButton()}
           </LinearGradient>
+          <Popover
+            backdropStyle={styles.backdrop}
+            visible={visible}
+            anchor={() => renderToggleButton()}
+            onBackdropPress={() => setVisible(false)}
+            style={{
+              marginBottom: 400,
+              maxWidth: 300,
+              height: 100,
+              padding: 20,
+              borderRadius: 10,
+            }}
+          >
+            <Layout style={styles.content}>
+              <Text>
+                There is no driver avaliable, we'll send you a notification when
+                one is ready👍
+              </Text>
+            </Layout>
+          </Popover>
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
   );
 };
 
+const styles = StyleSheet.create({
+  content: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+  },
+  avatar: {
+    marginHorizontal: 4,
+  },
+  backdrop: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+});
 export default RequestRide;
