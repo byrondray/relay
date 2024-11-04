6b1984b31b9e7d347ba7d75517f9f3db40c74124
JustArmaan <singharmaanbrar@gmail.com>
2024-11-04 10:55:17 -0800

    feature: create ui for friends list

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/Carpool/createRide` | `/(tabs)/Carpool/postRequest` | `/(tabs)/Community/community` | `/(tabs)/settings` | `/(tabs)/temp` | `/Carpool/createRide` | `/Carpool/postRequest` | `/Community/community` | `/Login/login` | `/OnboardForms/child` | `/OnboardForms/parent` | `/OnboardForms/vehicle` | `/Register/register` | `/_sitemap` | `/settings` | `/temp` | `/theme`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/Carpool/createRide` | `/(tabs)/Carpool/postRequest` | `/(tabs)/Community/community` | `/(tabs)/settings` | `/(tabs)/temp` | `/..\components\community\userMessages` | `/Carpool/createRide` | `/Carpool/postRequest` | `/Community/community` | `/Login/login` | `/OnboardForms/child` | `/OnboardForms/parent` | `/OnboardForms/vehicle` | `/Register/register` | `/_sitemap` | `/settings` | `/temp` | `/theme`;
       DynamicRoutes: `/(tabs)/messages/${Router.SingleRoutePart<T>}` | `/messages/${Router.SingleRoutePart<T>}`;
       DynamicRouteTemplate: `/(tabs)/messages/[userId]` | `/messages/[userId]`;
     }
--- app/(tabs)/Community/community.tsx
+++ app/(tabs)/Community/community.tsx
@@ -5,73 +5,70 @@ import {
   Layout,
   Text,
   Button,
+  Input,
+  Icon,
 } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
 import FriendButton from "@/assets/images/friendButton.svg";
 import PlusIcon from "@/assets/images/plusIcon.svg";
+import AddFriendsIcon from "@/assets/images/addFriendsIcon.svg";
+import SearchIcon from "@/assets/images/search.svg";
+import MessageIcon from "@/assets/images/message-square.svg";
+import UserMessageCard from "@/components/community/userMessages";
 
 const groups = [
   { id: 1, name: "Group 1", imageUrl: "https://picsum.photos/200" },
   { id: 2, name: "Group 2", imageUrl: "https://picsum.photos/200" },
   { id: 3, name: "Group 3", imageUrl: "https://picsum.photos/200" },
 ];
+const userProfiles = [
+  {
+    profilePicture: "https://picsum.photos/200",
+    userName: "John Doe",
+    latestMessage: "Hey! Are we still on for lunch tomorrow?",
+    notifications: 12,
+  },
+  {
+    profilePicture: "https://picsum.photos/200",
+    userName: "Jane Smith",
+    latestMessage: "Just finished the report, take a look!",
+    notifications: 1,
+  },
+  {
+    profilePicture: "https://picsum.photos/200",
+    userName: "Mike Johnson",
+    latestMessage: "Don't forget about the meeting at 3 PM.",
+    notifications: 5,
+  },
+];
 
 const Sidebar = () => (
-  <Layout
-    style={{
-      display: "flex",
-      width: 72,
-      height: 630,
-      paddingVertical: 19,
-      flexDirection: "column",
-      alignItems: "center",
-      backgroundColor: "rgba(251, 104, 86, 0.3)",
-      gap: 10,
-      flexShrink: 0,
-      borderRadius: 50,
-      marginLeft: 10,
-      marginTop: 19,
-    }}
-  >
-    <Button
+  <View>
+    <Layout
       style={{
-        width: 48,
-        height: 48,
-        justifyContent: "center",
+        display: "flex",
+        width: 72,
+        height: "84%",
+        paddingVertical: 19,
+        flexDirection: "column",
         alignItems: "center",
+        backgroundColor: "rgba(251, 104, 86, 0.3)",
+        gap: 10,
         flexShrink: 0,
-        backgroundColor: "#444",
-        borderRadius: 24,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 4,
+        borderRadius: 50,
+        marginLeft: 10,
+        marginTop: 19,
       }}
-      appearance="ghost"
-      accessoryLeft={() => <FriendButton width={48} height={48} />}
-      onPress={() => console.log(`Friends list`)}
-    />
-
-    <View
-      style={{
-        width: "45%",
-        height: 1,
-        backgroundColor: "#555",
-      }}
-    />
-
-    {groups.map((group) => (
+    >
       <Button
-        key={group.id}
         style={{
           width: 48,
           height: 48,
           justifyContent: "center",
           alignItems: "center",
           flexShrink: 0,
+          backgroundColor: "#444",
           borderRadius: 24,
-          marginBottom: 10,
           shadowColor: "#000",
           shadowOffset: { width: 0, height: 2 },
           shadowOpacity: 0.25,
@@ -79,55 +76,166 @@ const Sidebar = () => (
           elevation: 4,
         }}
         appearance="ghost"
-        accessoryLeft={() => (
-          <Image
-            source={{ uri: group.imageUrl }}
-            style={{
-              width: 40,
-              height: 40,
-              borderRadius: 20,
-            }}
-          />
-        )}
-        onPress={() => console.log(`Selected ${group.name}`)}
+        accessoryLeft={() => <FriendButton width={48} height={48} />}
+        onPress={() => console.log(`Friends list`)}
       />
-    ))}
-    <View
+
+      <View
+        style={{
+          width: "45%",
+          height: 1,
+          backgroundColor: "#555",
+        }}
+      />
+
+      {groups.map((group) => (
+        <Button
+          key={group.id}
+          style={{
+            width: 48,
+            height: 48,
+            justifyContent: "center",
+            alignItems: "center",
+            flexShrink: 0,
+            borderRadius: 24,
+            marginBottom: 10,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 4,
+          }}
+          appearance="ghost"
+          accessoryLeft={() => (
+            <Image
+              source={{ uri: group.imageUrl }}
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+              }}
+            />
+          )}
+          onPress={() => console.log(`Selected ${group.name}`)}
+        />
+      ))}
+      <View
+        style={{
+          width: "45%",
+          height: 1,
+          backgroundColor: "#555",
+        }}
+      />
+      <Button
+        style={{
+          width: 48,
+          height: 48,
+          padding: 4,
+          justifyContent: "center",
+          alignItems: "center",
+          flexShrink: 0,
+          borderRadius: 24,
+          backgroundColor: "#f5674d",
+          marginVertical: 8,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+          elevation: 4,
+        }}
+        appearance="ghost"
+        onPress={() => console.log(`Add a new group`)}
+        accessoryLeft={() => <PlusIcon width={24} height={24} />}
+      ></Button>
+    </Layout>
+    <Layout
       style={{
-        width: "45%",
-        height: 1,
-        backgroundColor: "#555",
-      }}
-    />
-    <Button
-      style={{
-        width: 48,
-        height: 48,
-        padding: 4,
+        width: 72,
+        height: 72,
         justifyContent: "center",
         alignItems: "center",
-        flexShrink: 0,
-        borderRadius: 24,
-        backgroundColor: "#f5674d",
-        marginVertical: 8,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 4,
+        borderRadius: 50,
+        backgroundColor: "rgba(251, 104, 86, 0.3)",
+        marginTop: 10,
+        marginBottom: 20,
+        marginLeft: 10,
       }}
-      appearance="ghost"
-      onPress={() => console.log(`Add a new group`)}
-      accessoryLeft={() => <PlusIcon width={24} height={24} />}
-    ></Button>
-  </Layout>
+    >
+      <Button
+        style={{
+          width: 48,
+          height: 48,
+          padding: 4,
+          justifyContent: "center",
+          alignItems: "center",
+          flexShrink: 0,
+          borderRadius: 24,
+          backgroundColor: "#f5674d",
+          marginVertical: 8,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+          elevation: 4,
+        }}
+        appearance="ghost"
+        onPress={() => console.log(`Add a new group`)}
+        accessoryLeft={() => <AddFriendsIcon width={24} height={24} />}
+      ></Button>
+    </Layout>
+  </View>
 );
 
 const Community = () => (
   <Layout style={{ flex: 1, flexDirection: "row" }}>
     <Sidebar />
     <Layout style={{ flex: 1, padding: 16, backgroundColor: "#f7f9fc" }}>
-      <Text category="h1">Welcome to the Community!</Text>
+      <Text category="h1">Friends</Text>
+
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginTop: 16,
+          width: "100%",
+        }}
+      >
+        <Input
+          placeholder="Search"
+          accessoryRight={() => <SearchIcon width={24} height={24} />}
+          style={{
+            flex: 1,
+            marginRight: 10,
+            backgroundColor: "#fff",
+            borderRadius: 24,
+          }}
+        />
+
+        <Button
+          style={{
+            width: 48,
+            height: 48,
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 24,
+            backgroundColor: "#f5674d",
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 4,
+          }}
+          appearance="ghost"
+          onPress={() => console.log("Start new message")}
+        >
+          <MessageIcon width={24} height={24} />
+        </Button>
+      </View>
+      <View style={{ marginTop: 20 }}>
+        {userProfiles.map((profile, index) => (
+          <UserMessageCard key={index} profile={profile} />
+        ))}
+      </View>
     </Layout>
   </Layout>
 );
