03c29ba190161e6145952cbab312d9fe90609261
JustArmaan <singharmaanbrar@gmail.com>
2024-11-08 22:15:25 -0800

    feature: show real data in friends component

--- app/(tabs)/Community/community.tsx
+++ app/(tabs)/Community/community.tsx
@@ -19,72 +19,74 @@ import UserMessageCard from "@/components/community/userMessages";
 import FriendsList from "@/components/community/FriendsList";
 import { GET_GROUPS } from "@/graphql/group/queries";
 import { useQuery } from "@apollo/client";
-import { Group } from "@/graphql/generated";
+import { FriendsWithUserInfo, Group } from "@/graphql/generated";
 import groupIcon from "@/assets/images/group-icon.svg";
+import { GET_FRIENDS } from "@/graphql/friends/queries";
+
 // const groups = [
 //   { id: 1, name: "Group 1", imageUrl: "https://picsum.photos/200" },
 //   { id: 2, name: "Group 2", imageUrl: "https://picsum.photos/200" },
 //   { id: 3, name: "Group 3", imageUrl: "https://picsum.photos/200" },
 // ];
 
-const userProfiles = [
-  {
-    profilePicture: "https://picsum.photos/200",
-    userName: "John Doe",
-    latestMessage: "Hey! Are we still on for lunch tomorrow?",
-    notifications: 12,
-    status: true,
-  },
-  {
-    profilePicture: "https://picsum.photos/200",
-    userName: "Jane Smith",
-    latestMessage: "Just finished the report, take a look!",
-    notifications: 1,
-    status: false,
-  },
-  {
-    profilePicture: "https://picsum.photos/200",
-    userName: "Mike Johnson",
-    latestMessage: "Don't forget about the meeting at 3 PM.",
-    notifications: 5,
-    status: false,
-  },
-  {
-    profilePicture: "https://picsum.photos/200",
-    userName: "Mike Johnson",
-    latestMessage: "Don't forget about the meeting at 3 PM.",
-    notifications: 5,
-    status: false,
-  },
-  {
-    profilePicture: "https://picsum.photos/200",
-    userName: "Mike Johnson",
-    latestMessage: "Don't forget about the meeting at 3 PM.",
-    notifications: 5,
-    status: true,
-  },
-  {
-    profilePicture: "https://picsum.photos/200",
-    userName: "Mike Johnson",
-    latestMessage: "Don't forget about the meeting at 3 PM.",
-    notifications: 5,
-    status: false,
-  },
-  {
-    profilePicture: "https://picsum.photos/200",
-    userName: "Mike Johnson",
-    latestMessage: "Don't forget about the meeting at 3 PM.",
-    notifications: 5,
-    status: true,
-  },
-  {
-    profilePicture: "https://picsum.photos/200",
-    userName: "Mike Johnson",
-    latestMessage: "Don't forget about the meeting at 3 PM.",
-    notifications: 5,
-    status: true,
-  },
-];
+// const userProfiles = [
+//   {
+//     profilePicture: "https://picsum.photos/200",
+//     userName: "John Doe",
+//     latestMessage: "Hey! Are we still on for lunch tomorrow?",
+//     notifications: 12,
+//     status: true,
+//   },
+//   {
+//     profilePicture: "https://picsum.photos/200",
+//     userName: "Jane Smith",
+//     latestMessage: "Just finished the report, take a look!",
+//     notifications: 1,
+//     status: false,
+//   },
+//   {
+//     profilePicture: "https://picsum.photos/200",
+//     userName: "Mike Johnson",
+//     latestMessage: "Don't forget about the meeting at 3 PM.",
+//     notifications: 5,
+//     status: false,
+//   },
+//   {
+//     profilePicture: "https://picsum.photos/200",
+//     userName: "Mike Johnson",
+//     latestMessage: "Don't forget about the meeting at 3 PM.",
+//     notifications: 5,
+//     status: false,
+//   },
+//   {
+//     profilePicture: "https://picsum.photos/200",
+//     userName: "Mike Johnson",
+//     latestMessage: "Don't forget about the meeting at 3 PM.",
+//     notifications: 5,
+//     status: true,
+//   },
+//   {
+//     profilePicture: "https://picsum.photos/200",
+//     userName: "Mike Johnson",
+//     latestMessage: "Don't forget about the meeting at 3 PM.",
+//     notifications: 5,
+//     status: false,
+//   },
+//   {
+//     profilePicture: "https://picsum.photos/200",
+//     userName: "Mike Johnson",
+//     latestMessage: "Don't forget about the meeting at 3 PM.",
+//     notifications: 5,
+//     status: true,
+//   },
+//   {
+//     profilePicture: "https://picsum.photos/200",
+//     userName: "Mike Johnson",
+//     latestMessage: "Don't forget about the meeting at 3 PM.",
+//     notifications: 5,
+//     status: true,
+//   },
+// ];
 
 const Sidebar = () => {
   const [groups, setGroups] = useState<Group[]>([]);
@@ -246,62 +248,76 @@ const Sidebar = () => {
   );
 };
 
-const Community = () => (
-  <Layout style={{ flex: 1, flexDirection: "row" }}>
-    <Sidebar />
-    <ScrollView style={{ flex: 1, padding: 16, backgroundColor: "#f7f9fc" }}>
-      <FriendsList profiles={userProfiles} />
-      <Text category="h1" style={{ marginTop: 0, fontFamily: "Comfortaa" }}>
-        Friends
-      </Text>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          marginTop: 16,
-          width: "100%",
-        }}
-      >
-        <Input
-          placeholder="Search"
-          accessoryRight={() => <SearchIcon width={24} height={24} />}
-          style={{
-            flex: 1,
-            marginRight: 10,
-            backgroundColor: "#fff",
-            borderRadius: 24,
-            fontFamily: "Comfortaa",
-          }}
-        />
+const Community = () => {
+  const [friends, setFriends] = useState<FriendsWithUserInfo[]>([]);
 
-        <Button
+  const { data: friendsData } = useQuery(GET_FRIENDS, {
+    onCompleted: (data) => {
+      console.log(data, "friends");
+      setFriends(data.getFriends);
+    },
+  });
+  return (
+    <Layout style={{ flex: 1, flexDirection: "row" }}>
+      <Sidebar />
+      <ScrollView style={{ flex: 1, padding: 16, backgroundColor: "#f7f9fc" }}>
+        <FriendsList profiles={friends} />
+        <Text category="h1" style={{ marginTop: 0, fontFamily: "Comfortaa" }}>
+          Friends
+        </Text>
+        <View
           style={{
-            width: 48,
-            height: 48,
-            justifyContent: "center",
+            flexDirection: "row",
             alignItems: "center",
-            borderRadius: 24,
-            backgroundColor: "#f5674d",
-            shadowColor: "#000",
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.25,
-            shadowRadius: 4,
-            elevation: 4,
+            marginTop: 16,
+            width: "100%",
           }}
-          appearance="ghost"
-          onPress={() => console.log("Start new message")}
         >
-          <MessageIcon width={24} height={24} />
-        </Button>
-      </View>
-      <View style={{ marginTop: 10 }}>
-        {userProfiles.map((profile, index) => (
-          <UserMessageCard key={index} profile={profile} />
-        ))}
-      </View>
-    </ScrollView>
-  </Layout>
-);
+          <Input
+            placeholder="Search"
+            accessoryRight={() => <SearchIcon width={24} height={24} />}
+            style={{
+              flex: 1,
+              marginRight: 10,
+              backgroundColor: "#fff",
+              borderRadius: 24,
+              fontFamily: "Comfortaa",
+            }}
+          />
+
+          <Button
+            style={{
+              width: 48,
+              height: 48,
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 24,
+              backgroundColor: "#f5674d",
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.25,
+              shadowRadius: 4,
+              elevation: 4,
+            }}
+            appearance="ghost"
+            onPress={() => console.log("Start new message")}
+          >
+            <MessageIcon width={24} height={24} />
+          </Button>
+        </View>
+        <View style={{ marginTop: 10 }}>
+          {friends.length > 0 ? (
+            friends.map((profile, index) => (
+              <UserMessageCard key={index} profile={profile} />
+            ))
+          ) : (
+            <Text>No friends available</Text>
+          )}
+        </View>
+      </ScrollView>
+    </Layout>
+  );
+};
 
 export default function App() {
   return (
--- graphql/friends/queries.ts
+++ graphql/friends/queries.ts
@@ -1,13 +1,45 @@
 import { gql } from "@apollo/client";
 
+// export const GET_FRIEND = gql`
+//   query GetFriend($userId: String!, $friendId: String!) {
+//     getFriend(userId: $userId, friendId: $friendId) {
+//       id
+//       userId
+//       createdAt
+//       friends {
+//         id
+//         firstName
+//         lastName
+//         email
+//         phoneNumber
+//         licenseImageUrl
+//         insuranceImageUrl
+//         city
+//         createdAt
+//         imageUrl
+//       }
+//     }
+//   }
+// `;
+
 export const GET_FRIENDS = gql`
-  query GetFriends($userId: String!) {
-    getFriends(userId: $userId) {
+  query GetFriends {
+    getFriends {
       id
-      firstName
-      lastName
-      email
-      imageUrl
+      userId
+      createdAt
+      friends {
+        id
+        firstName
+        lastName
+        email
+        phoneNumber
+        licenseImageUrl
+        insuranceImageUrl
+        city
+        createdAt
+        imageUrl
+      }
     }
   }
 `;
