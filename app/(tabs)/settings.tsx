637a6631b2c458cd2ef9ffb50c68530341086b58
byron dray <byrondray8@gmail.com>
2024-12-02 14:57:57 -0800

    fix: font on setting title

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -140,6 +140,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
+    fontFamily: "Comfortaa-bold"
   },
   settingItem: {
     flexDirection: "row",
